268a936f800148da387f902c71e768dff77b4705
tthao151196 <tthao151196@gmail.com>
2025-10-24 22:08:14 +0700

    Add new tailtailwin

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
-import tailwindcss from '@tailwindcss/vite'
+import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     laravel({
-      // Khớp với @vite trong Blade
       input: ['resources/css/app.css', 'resources/js/app.js'],
-      // Laravel sẽ tìm public/build/manifest.json
-      buildDirectory: 'build',
       refresh: true,
     }),
-    tailwindcss(),
+    tailwind(), // ✅ bắt Tailwind v4 scan & build
   ],
 })
